c952a0eb51b46c6fadf6bebc12242f5b59ec91a1
Dovilo <37475891+Dovilo@users.noreply.github.com>
2022-06-17 18:41:20 +0200

    Fixes

--- Data_Manipulation.py
+++ Data_Manipulation.py
@@ -6,9 +6,9 @@ import os
 today = datetime.now()
 
 #Recreate data downloaded in the Data_Loading.py.
-#If no data available or stale, exec that file
+#If no data available or stale, exec that file first
 HistDataList = {}
-FilteredPair = pd.read_csv('Data/_Pairs.csv', header=None).values.tolist()
+FilteredPair = pd.read_csv('Additional Data/_Pairs.csv', header=None).values.tolist()
 for i in range(len(os.listdir('Data'))):
     HistDataList[os.listdir('Data')[i].split('.')[0]] = pd.read_csv('Data/' + os.listdir('Data')[i])
 
@@ -17,8 +17,7 @@ for key in HistDataList.copy():
     if HistDataList.get(key).iloc[-1]['volumeto'] < 1000000:
         HistDataList.pop(key)
 
-#Remove crypto with little data
-datetime.timestamp(today)
+#Remove crypto with little historical data
 for key in HistDataList.copy():
     if today - HistDataList.get(key).iloc[0].name < timedelta(days = 365):
         HistDataList.pop(key)
