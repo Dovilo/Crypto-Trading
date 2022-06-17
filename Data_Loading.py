a3e10097cd8c1e3283eb52426ef3c6ce4641ce9c
Dovilo <37475891+Dovilo@users.noreply.github.com>
2022-06-17 18:24:45 +0200

    Add comments

--- Data_Loading.py
+++ Data_Loading.py
@@ -4,12 +4,14 @@ import pandas as pd
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 100)
 
+#Urls used for API requests
 urlHistData = "https://min-api.cryptocompare.com/data/v2/histoday"
 urlPairs = "https://min-api.cryptocompare.com/data/v2/pair/mapping/exchange"
 
 ApiKey = "c3002d0ec8ad7dbb7ab359e3530d32fc2b09e7b94f568f55c7080daf84bdbe2d"
 
 def Download_Pairs_Historical_Data(InPair, url, OutPair = 'USD', allData = True):
+    #A function to download historical data for cryptocurrencies from the API
     payload = {
         "fsym": InPair,
         "tsym": OutPair,
@@ -17,6 +19,8 @@ def Download_Pairs_Historical_Data(InPair, url, OutPair = 'USD', allData = True)
     }
     
     response = req.post(url, json=payload).json()
+    
+    #If for some reason the API sends an error back
     if response['Response'] == 'Error':
         return 'Error'
     else:
@@ -25,6 +29,8 @@ def Download_Pairs_Historical_Data(InPair, url, OutPair = 'USD', allData = True)
         return(SecondDF)
 
 def Download_Available_Pairs(url, ApiKey, Exchange = 'Binance'):
+    #A function to download all available crypto pairs on the provided exchange
+    #Seems to download more symbols than there is data for to download
     payload = {
         "e": Exchange,
         "api_key": ApiKey
@@ -35,12 +41,13 @@ def Download_Available_Pairs(url, ApiKey, Exchange = 'Binance'):
     SecondDF = pd.DataFrame(FirstDF['Data'][0])
     return(SecondDF)
 
+#Download available pairs and save them as csv for further use
 AvailablePairs = Download_Available_Pairs(urlPairs, ApiKey)
 FilteredPair = AvailablePairs[AvailablePairs['tsym'] == 'USDT']['fsym'].tolist()
 pd.DataFrame(FilteredPair).to_csv('Additional Data/_Pairs.csv', header=False, index=False)
 
+#Assign data frames with data to the dictionary with symbols as keys
 HistDataList = {}
-
 for i in FilteredPair:
     Download = Download_Pairs_Historical_Data(i, urlHistData)
     if isinstance(Download, pd.DataFrame):
--- Data_Manipulation.py
+++ Data_Manipulation.py
@@ -3,21 +3,22 @@ from datetime import datetime, timedelta
 import pandas as pd
 import os
 
-
 today = datetime.now()
+
+#Recreate data downloaded in the Data_Loading.py.
+#If no data available or stale, exec that file
 HistDataList = {}
 FilteredPair = pd.read_csv('Data/_Pairs.csv', header=None).values.tolist()
-
 for i in range(len(os.listdir('Data'))):
     HistDataList[os.listdir('Data')[i].split('.')[0]] = pd.read_csv('Data/' + os.listdir('Data')[i])
 
+#Remove crypto with small present volume
 for key in HistDataList.copy():
     if HistDataList.get(key).iloc[-1]['volumeto'] < 1000000:
         HistDataList.pop(key)
 
+#Remove crypto with little data
 datetime.timestamp(today)
-
 for key in HistDataList.copy():
     if today - HistDataList.get(key).iloc[0].name < timedelta(days = 365):
         HistDataList.pop(key)
-        
